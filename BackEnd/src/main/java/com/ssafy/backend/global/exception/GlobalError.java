b24ab7fc224351ba9cdb4d67bdadd06f8442b4aa
JungIn Park <wjddlsee@gmail.com>
2024-01-22 12:03:34 +0900

    fix: 인증, 인가 예외처리 문제 해결
    기존 예외처리의 경우 인가에러인 403에러 처리가 제대로 되지 않음
    Method security의 경우 ExceptionHandler를 이용하여 처리
    
    Resolves: #92

--- BackEnd/src/main/java/com/ssafy/backend/global/exception/GlobalError.java
+++ BackEnd/src/main/java/com/ssafy/backend/global/exception/GlobalError.java
@@ -9,7 +9,10 @@ import org.springframework.http.HttpStatus;
 public enum GlobalError {
     NOT_AUTHORITY_MEMBER_API(HttpStatus.FORBIDDEN, "해당 API 호출에 대한 권한이 없습니다."),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 인증 토큰입니다."),
-    CERTIFICATION_NOT_TOKEN(HttpStatus.UNAUTHORIZED, "자격 증명이 되어 있지 않은 토큰입니다.");
+    CERTIFICATION_NOT_TOKEN(HttpStatus.UNAUTHORIZED, "자격 증명이 되어 있지 않은 토큰입니다."),
+    FORBIDDEN_EXCEPTION(HttpStatus.FORBIDDEN, "권한이 없습니다."),
+    AUTHENTICATION_EXCEPTION(HttpStatus.UNAUTHORIZED, "인증되지 않은 요청입니다."),
+    DEFAULT_EXCEPTION(HttpStatus.BAD_REQUEST, "기본에러 입니다.");
 
     private final HttpStatus httpStatus;
     private final String errorMessage;
