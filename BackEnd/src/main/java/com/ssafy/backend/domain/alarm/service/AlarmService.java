b7b2e55feee2c49453339742bd43e6925d0646cd
박정인 <wjddlsee@gmail.com>
2024-01-30 19:42:57 +0900

    fix: 알람 목록조회 수정
    - SliceResponse 형태로 반환
    - 필요없는 join 제거
    
    Fixes: #S10P12C110-323 Related to: #S10P12C110-242

--- BackEnd/src/main/java/com/ssafy/backend/domain/alarm/service/AlarmService.java
+++ BackEnd/src/main/java/com/ssafy/backend/domain/alarm/service/AlarmService.java
@@ -2,6 +2,7 @@ package com.ssafy.backend.domain.alarm.service;
 
 import com.ssafy.backend.domain.alarm.dto.AlarmCreateRequestDto;
 import com.ssafy.backend.domain.alarm.dto.AlarmDto;
+import com.ssafy.backend.global.common.dto.SliceResponse;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
@@ -9,7 +10,7 @@ import java.util.List;
 public interface AlarmService {
     void createAlarm(Long fromMemberId, AlarmCreateRequestDto createRequestDto);
 
-    List<AlarmDto> getAlarmList(Long memberId, Pageable pageable);
+    SliceResponse getAlarmList(Long memberId, Pageable pageable);
 
     void acceptAlarm(Long memberId, Long alarmId);
 }
