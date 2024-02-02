95a3b56787118c9a81b483610103fb27b32876dc
박정인 <wjddlsee@gmail.com>
2024-02-02 09:27:54 +0900

    feat: 알림별 제목, 내용 저장

--- BackEnd/src/main/java/com/ssafy/backend/domain/alarm/entity/enums/AlarmType.java
+++ BackEnd/src/main/java/com/ssafy/backend/domain/alarm/entity/enums/AlarmType.java
@@ -1,5 +1,17 @@
 package com.ssafy.backend.domain.alarm.entity.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum AlarmType {
-    FRIEND, CONFERENCE
+    FRIEND("친구요청", "님이 친구요청"),
+    CONFERENCE("화상회의 초대", "화상회의 초대 요청");
+
+    private String title;
+    private String content;
+
+    private AlarmType(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
