43510d16674f04c6efe0c7fbe9896e5148164c45
박정인 <wjddlsee@gmail.com>
2024-02-13 15:02:15 +0900

    refactor: local, 배포환경에서 모두 적용되도록 수정
    - application.yml 파일에서 값을 가져오도록 수정

--- BackEnd/src/main/java/com/ssafy/backend/global/config/WebSocketConfig.java
+++ BackEnd/src/main/java/com/ssafy/backend/global/config/WebSocketConfig.java
@@ -28,7 +28,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.setApplicationDestinationPrefixes("/pub") // pub으로 시작되는 메시지는 브로커로 보내짐, 메시지를 publish하는 경로
                 .setUserDestinationPrefix("/users") // 특정 사용자에게 메시지 전송시 사용할 주소
                 .enableStompBrokerRelay("/queue", "/topic", "/exchange","/amq/queue") // subscribe 경로를 설정, 메시지 브로커에서 지원하는 접두사들
-                .setRelayHost("localhost")
+                .setRelayHost(rabbitMqProps.getHost())
                 .setVirtualHost("/")
                 .setRelayPort(61613) // RabbitMQ STOMP 기본 포트
                 .setSystemLogin(rabbitMqProps.getUsername())
